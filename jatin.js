8d5da7837205c256eed93f0080a3017706d03bbd
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-12-17 16:18:00 +0530

    .

---
+++ jatin.js
@@ -0,0 +1,13 @@
+function changeHeading() {
+    document.getElementById('heading').innerHTML = 'Jatssdev'
+    document.getElementById('heading').style.color = 'red'
+}
+function bulbOn() {
+    document.getElementById('bulbImage').src = 'https://www.w3schools.com/js/pic_bulbon.gif'
+}
+function bulbOff() {
+    document.getElementById('bulbImage').src = 'https://www.w3schools.com/js/pic_bulboff.gif'
+}
+function AnimateBox() {
+    document.getElementById('box').style.left = '400px'
+}
